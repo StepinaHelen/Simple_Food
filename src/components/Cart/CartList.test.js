7196f5d52975638d3919a7731944c78d7eee22ee
StepinaHelen <stopinahelen@gmail.com>
2022-12-16 09:05:21 +0100

    delete unnecessary

--- src/components/Cart/CartList.test.js
+++ src/components/Cart/CartList.test.js
@@ -1,12 +1,6 @@
 import { render } from "../../test-utils";
 import CartList from "./CartList";
 
-// const mockSetState = jest.fn();
-
-// jest.mock('react', () => ({
-//   useState: initial => [initial, mockSetState]
-// }));
-
 describe("CartList", () => {
   it("render CartList", () => {
     render(<CartList />);
