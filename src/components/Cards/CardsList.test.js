94601ae0e50101f2af08d6f3c23cb1674bc67a59
StepinaHelen <stopinahelen@gmail.com>
2022-12-16 20:35:14 +0100

    add tests for CardList and CartList

--- src/components/Cards/CardsList.test.js
+++ src/components/Cards/CardsList.test.js
@@ -1,8 +1,26 @@
-import { render } from "../../test-utils";
 import CardList from "./CardsList";
+import { render, screen } from "../../test-utils";
+import userEvent from "@testing-library/user-event";
+
+const mockTitle = "Noodles with shrimp";
 
 describe("cardList", () => {
-  it("render cardList", () => {
+  it("render cardList", async () => {
     render(<CardList />);
+    const text = await screen.findByText(mockTitle);
+    expect(text).toBeInTheDocument();
+  });
+  it("test click on sort", async () => {
+    render(<CardList />);
+    const sortBtn = await screen.findByTestId("sort");
+    await userEvent.click(sortBtn);
+    expect(JSON.parse(localStorage.getItem("sorting"))).toBeTruthy();
+  });
+
+  it("test click on filter setItem", async () => {
+    render(<CardList />);
+    const filterBtn = screen.getByRole("button", { name: "Pizza" });
+    await userEvent.click(filterBtn);
+    expect(JSON.parse(localStorage.getItem("category"))).toStrictEqual("Pizza");
   });
 });
--- src/components/Cart/CartList.test.js
+++ src/components/Cart/CartList.test.js
@@ -1,8 +1,69 @@
-import { render } from "../../test-utils";
+import { render, screen } from "../../test-utils";
+
 import CartList from "./CartList";
+
+import CartContext from "../../store/cart-context";
+
+const moсkedList = [
+  {
+    id: 15,
+    title: "Noodles with shrimp",
+    price: 25,
+    category: "noodles",
+    amount: 1,
+    img: "https://www.eatwell101.com/wp-content/uploads/2018/04/Shrimp-Zucchini-Noodles.jpg",
+  },
+
+  {
+    id: 16,
+    title: "Hawaiian pizza",
+    price: 25,
+    category: "pizza",
+    amount: 1,
+    img: "https://distfood.ru/upload/medialibrary/86a/86ab43892301045cb950f9874be29805.jpg",
+  },
+];
+
+const Wrapper = ({ children, removeItem, items }) => (
+  <CartContext.Provider
+    value={{
+      removeItem,
+      items,
+    }}
+  >
+    {children}
+  </CartContext.Provider>
+);
 
 describe("CartList", () => {
   it("render CartList", () => {
     render(<CartList />);
+  });
+
+  it("render Card list correctly", async () => {
+    render(
+      <Wrapper items={moсkedList}>
+        <CartList />
+      </Wrapper>
+    );
+
+    const text = await screen.findByText("Noodles with shrimp");
+
+    expect(text).toBeInTheDocument();
+  });
+
+  it("render message that we don't have any orders", async () => {
+    const moсkedList = [];
+    render(
+      <Wrapper items={moсkedList}>
+        <CartList />
+      </Wrapper>
+    );
+
+    const text = await screen.findByText(
+      "You don't have any items in your cart!"
+    );
+
+    expect(text).toBeInTheDocument();
   });
 });
